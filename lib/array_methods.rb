bf82a92fdbafedf0c296b6c4aaba88a009a029a8
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-17 21:40:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,10 @@
 def find_element_index(array, value_to_find)
-  #using Identify Elements
+  #using Identify Elements in an Array based on their Index in Array Basics Chapter of Programming Convo 4
   p array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+ 
 end
 
 def find_min_value(array)
